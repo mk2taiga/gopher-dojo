64d8287f0511f64a9169667bf71bec21d58bf3bc
zakimiiii <mk2.tg000@gmail.com>
2022-01-21 02:39:59 +0900

    feat(converter): add an1 interface

---
+++ kadai1/exchanger/converter/converter.go
@@ -0,0 +1,6 @@
+package converter
+
+// ConvertEtx converts the image files in the specified directories to specified extension.
+func ConvertEtx(src, from, to string) (int, error) {
+	return 0, nil
+}
